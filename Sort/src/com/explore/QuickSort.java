a725eea686a02ce5b2c1edf7099ca797e398cd25
b8d_100006157449_xiaohongbo <b8d_100006157449_xiaohongbo@git.cloud.tencent.com>
2018-11-23 18:04:58 +0800

    快速排序测试

--- Sort/src/com/explore/QuickSort.java
+++ Sort/src/com/explore/QuickSort.java
@@ -11,16 +11,25 @@ public class QuickSort {
     public static void sort(Comparable[] arr) {
         int n = arr.length;
         quickSort(arr,0,n-1);
-
+        SortTestHelper.printArray(arr);
     }
 
     private static void quickSort(Comparable[] arr, int l, int r) {
         if(l >= r){
             return;
         }
+
         int p = partition(arr,l,r);
+        SortTestHelper.printArray(arr);
+        System.out.println("partition p " + p);
+
+        System.out.println("partition l "+ l + " : " + (p-1));
         quickSort(arr,l,p-1);
+        SortTestHelper.printArray(arr);
+
+        System.out.println("partition r "+(p+1) + " : " + r);
         quickSort(arr,p+1,r);
+        SortTestHelper.printArray(arr);
 
     }
 
@@ -32,11 +41,12 @@ public class QuickSort {
             if(arr[i].compareTo(v) < 0){
                 j++;
                 swap(arr,j,i);
-
+                SortTestHelper.printArray(arr);
             }
         }
-        swap(arr,l,j);
 
+        swap(arr,l,j);
+        SortTestHelper.printArray(arr);
         return  j;
     }
 
@@ -48,7 +58,7 @@ public class QuickSort {
 
     public static void main(String[] args) {
         // TODO 思考为什么随机数的范围越小，快速排序的速度越慢？
-        Integer[] arr = SortTestHelper.generateRandomArray(100000,1,3);
+        Integer[] arr = SortTestHelper.generateRandomArray(20,1,5);
         //Integer[] arr = SortTestHelper.generateNearlyOrderArray(1000000,500);
         SortTestHelper.testSort("com.explore.QuickSort",arr);
         System.out.println(SortTestHelper.isSorted(arr));
---
+++ Stack/src/Test.java
@@ -0,0 +1,13 @@
+/**
+ * @ClassName Test
+ * @Description
+ * @Date 2018/11/14 15:11
+ * @Author Xiaohb
+ * @Version 1.0
+ **/
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println();
+    }
+}
